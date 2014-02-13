4ea7895a5736c3f4e9fe5013f6795efdb9f46fea
Joe Green <joe.green.web@gmail.com>
2014-02-13 12:42:51 +0000

    Added controller interface

---
+++ src/Smrtr/SpawnPoint/ControllerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Smrtr\SpawnPoint;
+
+/**
+ * Interface ControllerInterface
+ *
+ * @package Smrtr\SpawnPoint
+ * @author Joe Green
+ */
+interface ControllerInterface
+{
+    // Empty for now
+}
